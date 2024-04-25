98b73288b966f722af35ca7684f93937acca6a33
David Carbonari <davidcarbonari@MacBook-Air-di-David.local>
2024-04-25 17:36:14 +0200

    gestiti i campi contenenti array (ingredients, instructions) come elenchi tramite .map ed un nuovo componente List

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,17 @@
+import { List } from "./List";
+
 export const Card = ({ title, instructions, ingredients, image }) => {
+  console.log(ingredients[0]);
   return (
     <div className="flex flex-col m-3 p-5 text-center rounded-lg bg-slate-700 max-w-md">
       <div>
         <h2 className="text-red-500 font-thin text-3xl m-3">{title}</h2>
-        <p className="mb-2">Ingredients: {ingredients}</p>
+        <p className="mb-2">Ingredients: </p>
+        <ul className="flex flex-wrap list-disc m-3">
+          {ingredients.map((ingredient) => (
+            <List key={indexedDB}>{ingredient}</List>
+          ))}
+        </ul>
       </div>
 
       <div>
@@ -11,7 +19,11 @@ export const Card = ({ title, instructions, ingredients, image }) => {
       </div>
       <div>
         <h3 className="text-xl text-zinc-300 m-3">Instruction</h3>
-        <p className="text-zinc-400 m-3">{instructions}</p>
+        <ul className="text-zinc-400 m-3 list-disc text-left">
+          {instructions.map((instruction) => (
+            <List key={indexedDB}>{instruction}</List>
+          ))}
+        </ul>
       </div>
     </div>
   );
